0a14977a5364b5a2cf1736dd03aa0cd23d0f7da7
is688178 <is688178@gmail.com>
2021-11-29 02:15:12 -0600

    login?

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');  //You can use render in case of ejs 
 });
 
 module.exports = router;
